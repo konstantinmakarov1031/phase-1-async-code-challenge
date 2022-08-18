dcd517f117c56ef3728024e697cb1edb13eb9fbb
konstantin <makarovestates@gmail.com>
2022-08-17 20:12:08 -0500

    Made progress on title render

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code here
+function renderShows(title){
+    let showLi = document.createElement('li')
+    showLi.id = "listOfShows"
+    showLi.innerText = `${title.value}` 
+    document.getElementById('list').appendChild(showLi)
+    console.log(showLi)
+}
+
+
+
+function getAllShows(){
+    fetch('http://localhost:3000/shows')
+    .then(res => res.json())
+    .then(shows => {
+        let showName = shows.title
+        console.log(showName)
+        shows.forEach(show => renderShows(show))}     
+)}
+
+
+function initialize(){
+    getAllShows()
+}
+
+initialize();  
+
+
+
+
+
